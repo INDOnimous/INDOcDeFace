0df41ca32003215e8c22ecaff748b8c0ca7aed0f
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2019-10-31 08:28:28 +0700

    Create INDOcDeface.py

---
+++ INDOcDeface.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#creater by INDOnimous
+#website www.pasaronlen.com
+##### DONT CHANGE THIS..!!! #####
+import sys
+import os
+import platform
+import time
+import socket
+import random
+from time import*
+x = platform.system()
+import requests
+from tqdm import tqdm 
+#--- Code Color
+W  = '\033[0m'  # white (default)
+R  = '\033[31m' # red
+G  = '\033[1;32m' # green bold
+O  = '\033[33m' # orange
+B  = '\033[34m' # blue
+P  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+GR = '\033[37m' # gray
+
+now = strftime("%T")
+bulan = strftime("%B")
+tahun = strftime("%Y")
+###--- Def Menu
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+def banner():
+  	print 
+print(""+R+"Please "+G+" Do NOT "+R+"use this tool for"+B+" illegal activity")
+print(" [!] "+R+" Keep "+GR+"legal "+R+"don't illegal "+B+" [!]"+G+"")
+print
+os.system("")
+os.system("sleep 3")
+import sys
+import random
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.2)
+mengetik('Sebelum Anda Membuat Script Deface, Anda Akan di minta menginstal beberapa package yang di butuhkan.\nPackage Akan di install Secara Otomatis Oleh system kami. \n Silahkan Tunggu... \n')
+os.system("sleep 6")
+os.system("apt-get install ruby")
+os.system("gem install lolcat")
+os.system("apt-get install python")
+os.system("apt-get install python2")
+os.system("apt-get install wget")
+os.system("apt-get install unstable-repo")
+os.system("clear")
+def procedure():
+  time.sleep(5)
+def banner():
+  	print 
+print (""); os.system("date");os.system("whoami");
+print ("")
+print(""+R+"Please "+G+" Do NOT "+R+"use this tool for"+B+" illegal activity")
+print(" [!] "+R+" Keep "+GR+"legal "+R+"don't illegal "+B+" [!]"+G+"")
+print
+def procedure():
+	time.sleep(2.5)
+print
+print ""+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+W+""
+print "[ Website  ] : "+C+"https"+W+"://www.pasaronlen.com    "+O+"["+B+"+"+O+"]"+W+""
+print "[ Github   ] : "+C+"https"+W+"://github.com/INDOnimous "+O+"["+B+"+"+O+"]"
+print ""+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+O+"["+B+"+"+O+"]"+W+""
+print
+
+nama = raw_input("" "[ Input Your Name ] : " "")
+
+os.system("wget https://raw.githubusercontent.com/INDOnimous/INDOclon/master/INDOscript/INDOscript.py")
+os.system("mv INDOscript.py /data/data/com.termux/files/usr/bin/")
+os.system("chmod +x /data/data/com.termux/files/usr/bin/INDOscript.py")
+os.system("python2 /data/data/com.termux/files/usr/bin/INDOscript.py")
+
+
+
+
+
+
+
+
+
+
+
